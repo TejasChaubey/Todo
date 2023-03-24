fd802308e0b5bc42be313c167b739bd81fd38e9d
TejasChaubey <119616351+TejasChaubey@users.noreply.github.com>
2023-03-24 19:59:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const inputBox = document.getElementById("input-box");
+const listcontainer = document.getElementById("list-container");
+function addTask(){
+    if(inputBox.value ===''){
+        alert("You must write something.!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listcontainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value="";
+    saveData();
+}
+// this below script is for delete and check or uncheck the task 
+
+listcontainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+
+function saveData(){
+    localStorage.setItem("data",listcontainer.innerHTML);
+}
+
+function showData(){
+    listcontainer.innerHTML =localStorage.getItem("data");
+}
+showData();
+
